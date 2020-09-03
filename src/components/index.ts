ceae978751bca1a983ab99c77ac020e9286032fa
Łukasz Michalak <lukasz57570@gmail.com>
2020-09-03 17:17:40 +0200

    Added export Avatar

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './atoms/Button';
 export { default as RadioButton } from './atoms/RadioButton';
 export { default as Checkbox } from './atoms/Checkbox';
 export { default as ButtonLink } from './atoms/ButtonLink';
+export { default as Avatar } from './atoms/Avatar';
