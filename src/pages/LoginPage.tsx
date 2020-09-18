a7e82dfa4f1b4e6de742d244e241f2d9f94354d4
Łukasz Michalak <lukasz57570@gmail.com>
2020-09-18 16:39:40 +0200

    Added initial routes

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { LoginTemplate } from 'templates';
+
+export interface Props {}
+
+const LoginPage: React.FC<Props> = () => {
+  return <LoginTemplate />;
+};
+
+export default LoginPage;
---
+++ src/templates/LoginTemplate.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface Props {}
+
+const LoginTemplate: React.FC<Props> = () => {
+  return <div>login template</div>;
+};
+
+export default LoginTemplate;
